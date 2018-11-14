aa055ffbbdf0e13a4e5a7546ce43db438983fc7d
James Kon <konjames@gmail.com>
2018-11-13 20:12:15 -0600

    correcting the ref_sim name

--- test_everything.sh
+++ test_everything.sh
@@ -6,7 +6,7 @@ do
 	echo "$file" > my_results.txt
 	echo "$file" > ref_results.txt
 	./sim "$file" < run.txt >> my_results.txt
-	./ref_sim "$file" < run.txt >> ref_results.txt
+	./ref_sim_lab "$file" < run.txt >> ref_results.txt
 	diff my_results.txt ref_results.txt
 	rm my_results.txt
 	rm ref_results.txt
